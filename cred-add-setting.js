62e5a817900df496624ca2b977248d0816173715
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-01-27 22:29:03 +0900

    update

--- cred-add-setting.js
+++ cred-add-setting.js
@@ -5,6 +5,7 @@ module.exports = function (RED) {
   function CredAddSettingConfig(config) {
     RED.nodes.createNode(this, config);
     var node = this;
+    this.value1 = config.value1;
   }
   RED.nodes.registerType("cred-add-setting-config", CredAddSettingConfig, {
     credentials: {
@@ -16,16 +17,17 @@ module.exports = function (RED) {
   function CredAddSetting(config) {
     RED.nodes.createNode(this, config);
 
-    console.log(this);
-    console.log(config);
-
     this.current_setting = RED.nodes.getNode(config.setting);
+    this.value1 = this.current_setting.value1;
     this.apitoken = this.current_setting.credentials.apitoken;
     this.password = this.current_setting.credentials.password;
+
+    console.log("this.current_setting", this.current_setting);
 
     console.log("config.setting",config.setting);
     console.log("this.apitoken",this.apitoken);
     console.log("this.password",this.password);
+    console.log("this.value1",this.value1);
 
     var node = this;
     node.on('input', function (msg) {
